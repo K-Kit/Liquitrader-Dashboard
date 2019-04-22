1382643b2effc67bc9e513f90953b08648d8f743
K-Kit <kylekitlinski@gmail.com>
2019-04-21 21:50:19 -0400

    more theme stuff

---
+++ src/data/index.js
@@ -0,0 +1,62 @@
+export const dashboard_placeholder = {
+  quote_balance: "",
+  total_pending_value: "",
+  total_current_value: "",
+  market: "",
+  usd_balance_info: "/",
+  usd_total_profit: "",
+  usd_average_daily_gain: "",
+  market_change_24h: "",
+  average_daily_gain: "",
+  total_profit: "",
+  total_profit_percent: "",
+  daily_profit_data: [],
+  quote_candles: [],
+  holding_chart_data: [],
+  cum_profit: [],
+  market_conditions: [],
+  pair_profit_data: [],
+  recent_sales: []
+}
+
+export const lineData =  {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+}
+
+export const barData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
